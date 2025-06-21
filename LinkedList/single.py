04ca950043ed4e8b4275d80a1f18bd8637472e1a
fethulmubin <fetihul@gmail.com>
2025-06-21 12:58:33 +0300

    reverse linkedlist

--- LinkedList/single.py
+++ LinkedList/single.py
@@ -1,4 +1,20 @@
 
+# https://leetcode.com/problems/reverse-linked-list/
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev, cur = None, head
+        while cur:
+            nxt = cur.next
+            cur.next = prev
+            prev = cur
+            cur = nxt
+        return prev
+    
 # https://leetcode.com/problems/merge-two-sorted-lists/
 # Definition for singly-linked list.
 # class ListNode(object):
