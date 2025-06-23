f7ae9519b0f31aac856e628e316aba8ab74798b9
fethulmubin <fetihul@gmail.com>
2025-06-23 15:59:54 +0300

    implementing stack using queue

--- LinkedList/Queues.py
+++ LinkedList/Queues.py
@@ -14,3 +14,39 @@ class Solution(object):
             if count == len(students):
                 break
         return count
+    
+    # https://leetcode.com/problems/implement-stack-using-queues/description/
+    from collections import deque
+class MyStack(object):
+
+    def __init__(self):
+         self.queue = deque()
+        
+
+    def push(self, x):
+        self.queue.append(x)
+
+        for s in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+        
+        
+
+    def pop(self):
+       return self.queue.popleft()
+        
+
+    def top(self):
+        return self.queue[0]
+        
+
+    def empty(self):
+        return not self.queue
+        
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
