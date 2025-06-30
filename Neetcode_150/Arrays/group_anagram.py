869c34de56792886afc88c6b43fe8dc0625457c0
fethulmubin <fetihul@gmail.com>
2025-06-30 11:55:51 +0300

    grouped anagram added

---
+++ Neetcode_150/Arrays/group_anagram.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+class Solution(object):
+    def groupAnagrams(self, strs):
+        groupedAnagrams = defaultdict(list)
+        for word in strs:
+            count = [0] * 26
+            for char in word:
+                count[ord(char) - ord('a')] += 1
+            groupedAnagrams[tuple(count)].append(word)
+            print(groupedAnagrams[tuple(count)])
+        return list(groupedAnagrams.values())
+    
+# how it works:
+# 1. We use a defaultdict to group anagrams together.
+# 2. For each word, we create a count of characters (a list of size 26 for each letter in the alphabet).
+# 3. We convert this count to a tuple (to use it as a key in the dictionary) and append the word to the corresponding list.
+# 4. Finally, we return the values of the dictionary as a list of lists containing grouped anagrams.
+
+# groupedAnagrams seems like : 
+# {
+#  (1,0,0,0,1,0,...,1): ['eat', 'tea', 'ate'],
+#  (1,0,0,1,0,...,1): ['tan', 'nat'],
+#  (1,1,0,0,0,...,1): ['bat']
+# }
+sol = Solution()
+# Example usage:
+print(sol.groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]))
+# Output: [['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+# Explanation: The function groups the anagrams together in lists.
