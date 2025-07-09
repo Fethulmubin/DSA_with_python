7d9dd5fef4df845f43fcbf0a78d1e5adf84e0e48
fethulmubin <fetihul@gmail.com>
2025-07-09 20:44:49 +0300

    LCA least common ancesstor problem

--- Tree/BinarySearch_tree.py
+++ Tree/BinarySearch_tree.py
@@ -34,3 +34,30 @@ class Solution(object):
 # else:
 #     print("Not found")
 # This will output: Found: 2
+
+# https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+# The lowestCommonAncestor function finds the lowest common ancestor of two nodes (p and q) in a binary search tree (BST).
+# It works by comparing the values of p and q with the value of the current root node
+# (root). If both p and q are less than root, it recursively searches in the left subtree.
+# If both are greater, it searches in the right subtree. If one is on one side and the other is on the other side,
+# or if one of them is equal to the root, then the current root is the lowest common ancestor.
+# This function assumes that both p and q are present in the tree.
+# The time complexity is O(h), where h is the height of the tree, and the space complexity is O(h) due to the recursive call stack.
+# the else condition handles the case where they split this means 
+# p < root < q or p > root > q or one of them is equal to root
+# Example usage:
